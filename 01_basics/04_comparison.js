8bde31e61da4bf136a542c8d4f2fe501b2a26132
HalcyonAbhinav <coderabhinaviiitl@gmail.com>
2025-03-27 11:17:17 +0000

    comparison of dataTypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+console.log( 2 > 2 );
+console.log( 2 < 2 );
+console.log( 2 <= 2 );
+console.log( 2 >= 2 );
+console.log( 2 == 2 );
+console.log( 2 != 2 );
+
+
+console.log( "2" > 2 );//true (js automatically converts the string to number)
+console.log( "2" < 2 );//true (here also )
+
+
+
+console.log( null > 0 )//false
+console.log ( null == 0 )//false
+console.log ( null >= 0 )//true
+
+console.log( undefined > 0 )//false
+console.log ( undefined == 0 )//false
+console.log ( undefined >= 0 )//false
+
+
+//strict check ===
+
+
+
