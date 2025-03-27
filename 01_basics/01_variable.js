8f70f2557dca51abc896128bcb6aa27172d29892
HalcyonAbhinav <coderabhinaviiitl@gmail.com>
2025-03-27 09:18:28 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,43 @@
+const accountId = 12345 // constant ko declare krne ka ek hi trikha h jo ki h const varName
+
+
+//variable ko declare krne k do trike hai - let and var
+
+let accountEmail = "abhi@gmail.com" 
+
+var accountPassword = "1111"
+
+accountCity = "Lucknow"
+
+// accountId = 54321 (comment lines are not executed)
+
+
+accountEmail = "sagar@gmail.com"
+
+accountPassword = "2222"
+
+accountCity = "Bengaluru"
+
+
+
+
+// console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity])
+
+
+{
+
+    //This is scope (you will find in many thing )
+
+}
+
+//var does have control on scopes-
+//  So prefer not to use var becauze of issue in block scope and functional scope
+
+
+/*
+
+This is not good practice 
+e.g-> accountState = true;
+*/
