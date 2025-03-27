d6e96a1927a6651614cba1795e08e9a94c25c9eb
HalcyonAbhinav <coderabhinaviiitl@gmail.com>
2025-03-27 09:18:45 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict" //all JS code will be treated as the newer version 
+
+/* In old version javaScript doesnt have classes, arrow functions etc
+but newer version does have
+*/
+
+
+
+//------------------------------//
+
+
+/*
+Question arrives->
+1-Why alert(5+3) works on a browser and not in node js software
+
+*/
+
+//NOTE-> There must be a code readibility
+
+
+/*
+-------Primitive Data Types---------
+number => 2 to power 53
+bigint
+string
+boolean
+symbol(uniqueness)
+*/
+
+/*
+Special values->
+
+null(It is a standalone value )
+undefined
+*/
